a4b8e085b10d61c742a08a485523a01c6e5b4ca8
rajkp <rajkpatel@1728@gmail.com>
2024-06-11 19:43:49 +0530

    extract URL path

--- src/server.cpp
+++ src/server.cpp
@@ -8,7 +8,31 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int main(int argc, char **argv) {
+#include <vector>
+using namespace std;
+
+vector<string> split(string s)
+{
+  vector<string> ans;
+  string temp = "";
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (s[i] == ' ')
+    {
+      ans.push_back(temp);
+      temp = "";
+    }
+    else
+    {
+      temp += s[i];
+    }
+  }
+  ans.push_back(temp);
+  return ans;
+}
+
+int main(int argc, char **argv)
+{
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -19,15 +43,17 @@ int main(int argc, char **argv) {
   // Uncomment this block to pass the first stage
   //
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+  if (server_fd < 0)
+  {
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
   //
   // // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+  {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
@@ -37,13 +63,15 @@ int main(int argc, char **argv) {
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(4221);
 
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+  {
     std::cerr << "Failed to bind to port 4221\n";
     return 1;
   }
 
   int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
+  if (listen(server_fd, connection_backlog) != 0)
+  {
     std::cerr << "listen failed\n";
     return 1;
   }
@@ -53,10 +81,27 @@ int main(int argc, char **argv) {
 
   std::cout << "Waiting for a client to connect...\n";
 
-  int clientfd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int clientfd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
   std::cout << "Client connected\n";
 
-  std::string response =  "HTTP/1.1 200 OK\r\n\r\n";
+  char req[1024];
+  std::string response = "HTTP/1.1 200 OK\r\n\r\n";
+
+  recv(clientfd, req, 1024, 0);
+  std::cout << req << "\n";
+  std::string req_str(req);
+
+  vector<string> req_split = split(req_str);
+  string path = req_split[1];
+  cout << path << endl;
+  if (path == "/")
+  {
+    response = "HTTP/1.1 200 OK\r\n\r\n";
+  }
+  else
+  {
+    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+  }
   send(clientfd, response.c_str(), response.size(), 0);
 
   close(server_fd);
